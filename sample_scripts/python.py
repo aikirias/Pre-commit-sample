f9fd6e22a2b6f053d1da70243bc33aebedcc71f3
Alejandro Kwiek <a.kwiek@ptytechnologies.com>
2022-11-25 12:35:40 -0300

    adding all the examples

--- sample_scripts/python.py
+++ sample_scripts/python.py
@@ -1,13 +1,25 @@
 from __future__ import unicode_literals, print_function
-import os.path as ospath  # ospath is great
 from package.subpackage.module.submodule import CONSTANT, Klass, foo, bar, rainbows
-# UTC all the things
 import datetime # pytz
-from .module import foo, bar  # baz
-from ..othermodule import rainbows
+import pandas as pd
+import numpy as np
+from my_lib import Object
 
-from seven_dwwarfs import Grumpy, Happy, Sleepy, Bashful, Sneezy, Dopey, Doc
-from seven_dwwarfs import NewDwwarf
+import os
+
+from my_lib import Object3
+
+from my_lib import Object2
+
+import sys
+
+from third_party import lib15, lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9, lib10, lib11, lib12, lib13, lib14
+
+import sys
+
+from __future__ import absolute_import
+
+from third_party import lib3
 x = {  'a':37,'b':42,
 
 'c':927}
